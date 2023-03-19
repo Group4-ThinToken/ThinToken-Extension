c862280f463602a484448ede9fa5207b385a555e
James <cruz.james99@gmail.com>
2023-03-19 15:50:24 +0800

    Change accountList.js to 2 space indents

--- extension/AccountList/accountList.js
+++ extension/AccountList/accountList.js
@@ -1,59 +1,58 @@
 function getIconClass(accountName) {
-    const iconClasses = {
-      Facebook: "fab fa-facebook",
-      Google: "fab fa-google",
-      Twitter: "fab fa-twitter",
-    };
-  
-    return iconClasses[accountName] || "fas fa-question-circle";
+  const iconClasses = {
+    Facebook: "fab fa-facebook",
+    Google: "fab fa-google",
+    Twitter: "fab fa-twitter",
+  };
+
+  return iconClasses[accountName] || "fas fa-question-circle";
+}
+
+function createAccountCard(account) {
+  const card = document.createElement("div");
+  card.className = "accountCard";
+
+  const iconContainer = document.createElement("div");
+  iconContainer.className = "icon";
+  const icon = document.createElement("i");
+  icon.className = getIconClass(account.accountName);
+  iconContainer.appendChild(icon);
+  card.appendChild(iconContainer);
+
+  const accountName = document.createElement("div");
+  accountName.className = "accountName";
+  accountName.textContent = account.accountName;
+  card.appendChild(accountName);
+
+  const accountEmail = document.createElement("div");
+  accountEmail.className = "accountEmail";
+  accountEmail.textContent = account.accountEmail;
+  card.appendChild(accountEmail);
+
+  return card;
+}
+
+function displayAccounts(accounts) {
+  const accountListing = document.getElementById("accountlisting");
+  accounts.forEach(account => {
+    const accountCard = createAccountCard(account);
+    accountListing.appendChild(accountCard);
+  });
+}
+
+const accounts = [
+  {
+    accountName: "Facebook",
+    accountEmail: "example@facebook.com",
+  },
+  {
+    accountName: "Google",
+    accountEmail: "example@google.com",
+  },
+  {
+    accountName: "Twitter",
+    accountEmail: "example@twitter.com",
   }
-  
-  function createAccountCard(account) {
-    const card = document.createElement("div");
-    card.className = "accountCard";
-  
-    const iconContainer = document.createElement("div");
-    iconContainer.className = "icon";
-    const icon = document.createElement("i");
-    icon.className = getIconClass(account.accountName);
-    iconContainer.appendChild(icon);
-    card.appendChild(iconContainer);
-  
-    const accountName = document.createElement("div");
-    accountName.className = "accountName";
-    accountName.textContent = account.accountName;
-    card.appendChild(accountName);
-  
-    const accountEmail = document.createElement("div");
-    accountEmail.className = "accountEmail";
-    accountEmail.textContent = account.accountEmail;
-    card.appendChild(accountEmail);
-  
-    return card;
-  }
-  
-  function displayAccounts(accounts) {
-    const accountListing = document.getElementById("accountlisting");
-    accounts.forEach(account => {
-      const accountCard = createAccountCard(account);
-      accountListing.appendChild(accountCard);
-    });
-  }
-  
-  const accounts = [
-    {
-      accountName: "Facebook",
-      accountEmail: "example@facebook.com",
-    },
-    {
-      accountName: "Google",
-      accountEmail: "example@google.com",
-    },
-    {
-      accountName: "Twitter",
-      accountEmail: "example@twitter.com",
-    }
-  ];
-  
-  displayAccounts(accounts);
-  
+];
+
+displayAccounts(accounts);
